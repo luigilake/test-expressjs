64991b74b1695cbe25845d0e1cd1cc37eef8d7c6
Luigi Lake <lfdlake@gmail.com>
2018-01-04 15:36:24 -0500

    initialize logger for middleware

--- app.js
+++ app.js
@@ -3,6 +3,12 @@ import bodyParser from 'body-parser';
 import path from 'path';
 
 var app = express();
+var logger = (request, response, next) => {
+  console.log('Logging...');
+  next();
+}
+
+app.use(logger);
 
 app.get('/', (request, response) => {
   response.send('Hello World');
