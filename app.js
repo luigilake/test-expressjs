0dfffde1860955c7f0aad3e9a181ece745a1b2ff
Luigi Lake <lfdlake@gmail.com>
2018-01-04 15:39:26 -0500

    initialize body parser middleware and set static path

--- app.js
+++ app.js
@@ -2,13 +2,20 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import path from 'path';
 
-var app = express();
-var logger = (request, response, next) => {
-  console.log('Logging...');
-  next();
-}
+// var app = express();
+// var logger = (request, response, next) => {
+//   console.log('Logging...');
+//   next();
+// }
+//
+// app.use(logger);
 
-app.use(logger);
+// body parser middleware
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}))
+
+// set static path
+app.use(express.static(path.join(__dirname, 'public'))); // css files or static resources go into public folder
 
 app.get('/', (request, response) => {
   response.send('Hello World');
