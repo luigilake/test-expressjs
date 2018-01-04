0ce3f0a275943c19f90f25dc63f8e60e3e5e872e
Luigi Lake <lfdlake@gmail.com>
2018-01-04 15:44:06 -0500

    delete index html to render views from server

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ import bodyParser from 'body-parser';
 import path from 'path';
 
 var app = express();
+
 // var logger = (request, response, next) => {
 //   console.log('Logging...');
 //   next();
@@ -15,7 +16,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}))
 
 // set static path
-app.use(express.static(path.join(__dirname, 'public'))); // css files or static resources go into public folder
+app.use(express.static(path.join(__dirname, 'public'))); // css files, react, angular, or static resources go into public folder
 
 app.get('/', (request, response) => {
   response.send('Hello World');
