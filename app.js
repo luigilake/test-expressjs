a0ab919f7748baaffedbd5ea8767ac0f7c0ff2f7
Luigi Lake <lfdlake@gmail.com>
2018-01-04 15:31:01 -0500

    add response text to localhost

--- app.js
+++ app.js
@@ -4,6 +4,10 @@ import path from 'path';
 
 var app = express();
 
+app.get('/', (request, response) => {
+  response.send('Hello World');
+});
+
 app.listen(3000, () => {
   console.log('Server started on Port 3000...')
-})
+});
